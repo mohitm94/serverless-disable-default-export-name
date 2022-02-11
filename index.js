ae31de12cab5714b47851e2ac5f09e127c3e5af2
Mohit Mittal <mohitm@wayforward.io>
2022-02-11 11:43:49 +0530

    Fix to properly delete the name and cleanup

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ class AWSExportNames {
         this.serverless = serverless;
         this.service = serverless.service;
         this.hooks = {
-            'before:package:finalize': this.disableDefaultOutputExportNames.bind(this)
+            'aws:package:finalize:mergeCustomProviderResources': this.disableDefaultOutputExportNames.bind(this)
         }
-
     }
 
     disableDefaultOutputExportNames() {
         const cfnTemplate = this.serverless.service.provider.compiledCloudFormationTemplate;
-
         for (const [key, data] of Object.entries(cfnTemplate.Outputs)) {
             if (data.Export !== undefined) {
                 delete data.Export;
